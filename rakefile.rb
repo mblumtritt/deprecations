b3b28437e99196c1b0644d924fa7a3c97b3c61b8
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2022-07-30 19:46:10 +0200

    Tidy-up Rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,15 +1,11 @@
+# frozen_string_literal: true
+
 require 'rake/clean'
 require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
 
 $stdout.sync = $stderr.sync = true
 
+CLOBBER << 'prj'
 task(:default) { exec('rake --tasks') }
-
-RSpec::Core::RakeTask.new do |t|
-  t.pattern = %w[spec/*_spec.rb spec/**/*/*_spec.rb]
-  t.rspec_opts = '-w -fd'
-  t.verbose = Rake.application.options.trace
-end
-
-task test: :spec
+RSpec::Core::RakeTask.new(:test) { |task| task.ruby_opts = %w[-w] }
